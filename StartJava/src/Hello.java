ceac94d8cc381d5852545b26a39b3599dc97cdfd
jmattew <jacobdotmathew@gmail.com>
2022-11-03 22:19:07 -0400

    Test HashMap data structure
    Test HashMap data structure

--- StartJava/src/Hello.java
+++ StartJava/src/Hello.java
@@ -1,6 +1,17 @@
+import java.util.*;
 public class Hello {
     public static void main(String[] args)
     {
-        System.out.println("Yo, I am Jacob!");
+        HashMap<Integer, Integer> x = new HashMap<Integer, Integer>();
+        int val = 35;
+        for(int y = 0; y<10;y++)
+        {
+            x.put(y, val);
+            val++;
+        }
+        for(int i:x.keySet())
+        {
+            System.out.println("key i is: " + i + " value y is: " + x.get(i));
+        }
     }
 }
